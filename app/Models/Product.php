f41b8a80709184e7413b6a6eb965e519ca2e0507
nkport <ktmtworks@outlook.jp>
2024-02-04 15:13:30 +0900

    「作成日時によるソート機能を追加しよう」課題提出

---
+++ app/Models/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Kyslik\ColumnSortable\Sortable;
+
+class Product extends Model
+{
+    use HasFactory, Sortable;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function favorite_products()
+    {
+        return $this->belongsToMany(Product::class)->withTimestamps();
+    }
+
+}
